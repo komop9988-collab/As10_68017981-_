78f36577ac9ddbde345dcd5b98a525e9e5a3293e
komop9988-collab <komop9988@gmail.com>
2026-03-17 17:09:59 +0700

    Add files via upload

---
+++ myapp/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from myapp.models import Person
+
+def index(request):
+    all_Person = Person.objects.all()
+    return render(request,"index.html",{"all_person":all_Person})
+
+def about(request):
+    return render(request,"about.html")
+
+def form(request):
+    return render(request,"form.html")
